3ceeb6c55e7603d0a1ab746f0a7bbff58fa7d701
nastyagurskaya <just.gurskaya@gmail.com>
2018-03-17 02:27:31 +0300

    Добавьте файлы проекта.

---
+++ TestTask/Controllers/ShopsController.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TestTask.Models;
+
+namespace TestTask.Controllers
+{
+    public class ShopsController : Controller
+    {
+        private ShopDBEntities db = new ShopDBEntities();
+      
+        // GET: Shops
+        public ActionResult Index()
+        {
+            return View(db.Shop.ToList());
+        }
+        public ActionResult ViewItems(int? id)
+        {
+            if (id == null)
+            {
+                id = ViewBag.SelectedID;
+            }
+            else ViewBag.SelectedID = id;
+            var items = db.Item.Include(i => i.Shop);
+            items = items.Where(s => s.ShopID == id);
+            return View(items.ToList());
+        }
+        // GET: Shops/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Shop shop = db.Shop.Find(id);
+            if (shop == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shop);
+        }
+
+        // GET: Shops/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Shops/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Address,WorkingHours")] Shop shop)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Shop.Add(shop);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(shop);
+        }
+
+        // GET: Shops/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Shop shop = db.Shop.Find(id);
+            if (shop == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shop);
+        }
+
+        // POST: Shops/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Address,WorkingHours")] Shop shop)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(shop).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(shop);
+        }
+
+        // GET: Shops/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Shop shop = db.Shop.Find(id);
+            if (shop == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shop);
+        }
+
+        // POST: Shops/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Shop shop = db.Shop.Find(id);
+            db.Shop.Remove(shop);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        // GET: Items/Details/5
+        public ActionResult DetailsItem(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Item.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        // GET: Items/Create
+        public ActionResult CreateItem()
+        {
+            ViewBag.ShopID = new SelectList(db.Shop, "Id", "Name");
+            return View();
+        }
+
+        // POST: Items/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateItem([Bind(Include = "Id,Name,Description,ShopID")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Item.Add(item);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ShopID = new SelectList(db.Shop, "Id", "Name", item.ShopID);
+            return View(item);
+        }
+
+        // GET: Items/Edit/5
+        public ActionResult EditItem(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Item.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ShopID = new SelectList(db.Shop, "Id", "Name", item.ShopID);
+            return View(item);
+        }
+
+        // POST: Items/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditItem([Bind(Include = "Id,Name,Description,ShopID")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(item).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ShopID = new SelectList(db.Shop, "Id", "Name", item.ShopID);
+            return View(item);
+        }
+
+        // GET: Items/Delete/5
+        public ActionResult DeleteItem(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Item.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        // POST: Items/Delete/5
+        [HttpPost, ActionName("DeleteItem")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteItemConfirmed(int id)
+        {
+            Item item = db.Item.Find(id);
+            db.Item.Remove(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
